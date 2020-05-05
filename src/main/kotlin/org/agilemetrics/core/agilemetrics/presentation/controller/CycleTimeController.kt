4c8fe8d0f4e880679a7c01848f3f4298c871d1ee
Carlos Camacho <carlos.camacho.gonzalez@ing.com>
2020-05-05 08:23:34 +0200

    code style and return cycletimeout in the controller

--- src/main/kotlin/org/agilemetrics/core/agilemetrics/presentation/controller/CycleTimeController.kt
+++ src/main/kotlin/org/agilemetrics/core/agilemetrics/presentation/controller/CycleTimeController.kt
@@ -1,6 +1,7 @@
 package org.agilemetrics.core.agilemetrics.presentation.controller
 
 import org.agilemetrics.core.agilemetrics.presentation.controller.dto.CycleTimeIn
+import org.agilemetrics.core.agilemetrics.presentation.controller.dto.CycleTimeOut
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,5 +11,6 @@ import org.springframework.web.bind.annotation.RestController
 class CycleTimeController() {
 
     @PostMapping("/cycle-time")
-    fun saveCycleTime(cycleTime: CycleTimeIn) = null;
+    fun saveCycleTime(cycleTime: CycleTimeIn): CycleTimeOut? = null
+
 }
