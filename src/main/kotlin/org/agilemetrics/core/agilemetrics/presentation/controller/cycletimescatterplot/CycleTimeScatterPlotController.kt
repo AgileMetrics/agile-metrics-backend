7e3f39fac7f9dfe2773c8091ed5cbbf01a469556
Carlos Camacho <ccamachog@gmail.com>
2020-06-14 14:00:23 +0200

    add crossorigin support in cycletime controller to test our frontend

--- src/main/kotlin/org/agilemetrics/core/agilemetrics/presentation/controller/cycletimescatterplot/CycleTimeScatterPlotController.kt
+++ src/main/kotlin/org/agilemetrics/core/agilemetrics/presentation/controller/cycletimescatterplot/CycleTimeScatterPlotController.kt
@@ -1,6 +1,7 @@
 package org.agilemetrics.core.agilemetrics.presentation.controller.cycletimescatterplot
 
 import org.agilemetrics.core.agilemetrics.business.service.CycleTimeScatterPlotService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,6 +13,7 @@ class CycleTimeScatterPlotController(
 ) {
 
     @GetMapping("/cycle-time-scatterplot")
+    @CrossOrigin
     fun getCycleTimeScatterPlot() = service.getValues()
 
 }
